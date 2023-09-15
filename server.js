a71f23bb0ee4f967820053b9df97b4f50d001437
Venkatesh R <venkatesh12102k2@gmail.com>
2023-09-15 22:17:11 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv');
 const colors = require('colors');
 const cors = require('cors');
 const morgan = require('morgan');
-
+const path = require('path')
 const connectDB = require('./config/db');
 
 dotenv.config();
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
- 
+
 app.use('/api/v1/test', require('./routes/testRoute'));
 app.use('/api/v1/auth', require('./routes/authRoutes'));
 app.use('/api/v1/inventory', require('./routes/inventoryRoutes'));
@@ -25,8 +25,16 @@ app.use('/api/v1/analytics', require('./routes/analyticsRoutes'));
 app.use('/api/v1/admin', require('./routes/adminRoutes'));
 
 
+//Static Folder
+app.use(express.static(path.join(__dirname, './client/build')));
+
+//Static Routes
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"))
+})
+
 const PORT = process.env.PORT || 4147;
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Node Server is Running in ${process.env.DEV_MODE} ON Port ${process.env.PORT || 4147}`.bgBlue.white);
 });
