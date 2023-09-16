f719bbc7cbeba9318f30c3f78a1e2dd3110b856c
Venkatesh R <venkatesh12102k2@gmail.com>
2023-09-16 13:22:16 +0530

    forgot to update build :-( now doing tha

--- client/src/components/shared/Layout/Header.js
+++ client/src/components/shared/Layout/Header.js
@@ -4,7 +4,7 @@ import { useSelector } from 'react-redux'
 import { useNavigate, Link, useLocation } from 'react-router-dom'
 import { toast } from 'react-toastify'
 const Header = () => {
-    const { user } = useSelector((state) => state.auth)
+    const { user } = useSelector(state => state.auth)
     const navigate = useNavigate()
     const location = useLocation()
 
@@ -22,11 +22,11 @@ const Header = () => {
                     <div className='navbar-brand h1'><BiDonateBlood color='red' /> Blood Bank App</div>
                     <ul className='navbar-nav flex-row'>
                         <li className='nav-item mx-3'>
-                            {/* <p className='nav-link'><BiUserCircle />Welcome {user?.name || user?.hospitalName || user?.organisationName}  <span className='badge bg-secondary'>{user?.role}</span></p> */}
+                            <p className='nav-link'><BiUserCircle />Welcome {user?.name || user?.hospitalName || user?.organisationName}  <span className='badge bg-secondary'>{user?.role}</span></p>
 
                         </li>
 
-                        {(location.pathname === "/" || location.pathname === "/donar" || location.pathname === "/hospital") ? (
+                        {(user?.role === "organisation") && (location.pathname === "/" || location.pathname === "/donar" || location.pathname === "/hospital") ? (
                             <li className='nav-item mx-3'>
                                 <Link to="/analytics" className='nav-link'>
                                     Analytics
